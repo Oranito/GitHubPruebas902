6c25cd3b5b5bc805aabe5682a318adfe799ab88f
SergioDLC <5721100032@utrng.edu.mx>
2024-06-01 20:16:57 -0600

    Se agrego un nuevo controlador

---
+++ ExamenDDI_Sergio/ExamenDDI_Ale/Controllers/MovieCallController.cs
@@ -0,0 +1,28 @@
+﻿using ExamenDDI_Ale.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ExamenDDI_Ale.Controllers
+{
+    public class MovieCallController : Controller
+    {
+        private readonly TMDbService _tmdbService;
+
+        public MovieCallController(TMDbService tmdbService)
+        {
+            _tmdbService = tmdbService;
+        }
+
+        public async Task<IActionResult> NowPlaying()
+        {
+            var movies = await _tmdbService.GetNowPlayingMoviesAsync();
+            return View(movies);
+        }
+    }
+}
+
+/* 
+APB Ardillas por doquier
+
+ven mas cerca y mirarme bailar
+*/
